1e3362e0c1a14b9ef2e5724b34b0fb62d2d7ca86
Ken Akiha <95503833+kakiha11@users.noreply.github.com>
2025-12-17 19:52:01 -0800

    Initial commit - exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function GuessInput() {
+  const [inputValue, setInputValue] = React.useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    // Process the form submission here
+    console.log('Form submitted with value:', inputValue);
+    
+    // Clear the input after submission
+    setInputValue('');
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value.toUpperCase());
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className='guess-input-wrapper'>
+      <label htmlFor="guess-input">
+        Enter guess:
+      </label>
+      <input
+        id="guess-input"
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter your guess"
+        required
+        pattern="[A-Za-z]{5}"
+        title="Please enter exactly 5 letters."
+        minLength={5}
+        maxLength={5}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
